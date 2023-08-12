1c0908f74eb77c22798205d0ce528ab266528521
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-08-13 02:39:06 +0300

    Setuped commander

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # goit-node-hw-01-contacts
+
 Модуль 1. Основи Node.js.
+
+<!-- node index.js --action list -->
+
+https://monosnap.com/file/LqH4x7O345fKBRJl835TnHwwtnjNcH
+
+<!-- node index.js --action get --id 05olLMgyVQdWRwgKfg5J6 -->
+
+https://monosnap.com/file/JifCUE370vjUZu9Cy9ScKgCaRXXyF7
+
+<!-- node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22 -->
+
+https://monosnap.com/file/bO5rMV5EOTJtWN8vL7iZb1VhGkGzco
+
+<!-- node index.js --action remove --id qdggE76Jtbfd9eWJHrssH -->
+
+https://monosnap.com/file/8oMGktHWyh5inunK3LLP240SpecE0d
--- index.js
+++ index.js
@@ -1,50 +1,39 @@
 const contacts = require("./contacts");
 
-// const { Command } = require("commander");
-// const program = new Command();
-// program
-//   .option("-a, --action <type>", "choose action")
-//   .option("-i, --id <type>", "user id")
-//   .option("-n, --name <type>", "user name")
-//   .option("-e, --email <type>", "user email")
-//   .option("-p, --phone <type>", "user phone");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-// program.parse(process.argv);
+program.parse(process.argv);
 
-// const argv = program.opts();
-
-// TODO: рефакторить
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const listContacts = await contacts.listContacts();
       return console.table(listContacts);
-      break;
 
     case "get":
       const getContact = await contacts.getContactById(id);
       return console.log(getContact);
-      break;
 
     case "add":
       const addContact = await contacts.addContact({ name, email, phone });
       return console.log(addContact);
-      break;
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
       return console.log(removeContact);
-      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-invokeAction({
-  action: "remove",
-  id: "qdggE76Jtbfd9eWJHrssH",
-}); //!
-
-// invokeAction(argv);
+invokeAction(argv);
